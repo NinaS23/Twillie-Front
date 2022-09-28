7013c0257e5ad84d2c5636ebacea9a996a0cf4e9
MariaDuda <mariabevenuto062@gmail.com>
2022-09-28 12:26:04 -0300

    [Feat] login css

---
+++ src/components/FormsLoginSigin.js
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+export default function FormsLoginSigin({
+    type
+}) {
+    if (type === "register")
+        return (
+            <>
+                <h1>registro</h1>
+            </>
+        )
+    if (type === "login") {
+        return (
+            <>
+                <Input placeholder="email" type="email" ></Input>
+                <Input placeholder="password" type="password" ></Input>
+                <ButtonRegister>
+                    <h3>
+                        Log in
+                    </h3>
+                </ButtonRegister>
+                <SubTitle>
+                    <h4>still don't have an account? click here and sign up</h4>
+                </SubTitle>
+            </>
+        )
+    }
+
+}
+
+
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+height: 295px;
+padding-top: 20%;
+`
+const Input = styled.input`
+   height: 30px;
+   width: 100%;
+    margin-top: 8%;
+    background-color: aliceblue;
+    border-style: outset;
+  border-color: #5897D6;
+  border-width: 1px;
+  ::placeholder{
+
+    font-family: 'kanit', serif;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 10px;
+  }
+
+`
+const RedirectRoute = styled.div`
+a{
+  justify-content: center;
+  color: #FFFFFF;
+  font-size: 20px;
+  font-weight: 300;
+  
+}
+
+`
+
+const ButtonRegister = styled.button`
+ height: 30px;
+ width: 80%;
+ margin-left: 10%;
+ margin-top: 10%;
+ background-color: #5897D6;
+  border-style: none;
+  &:hover {
+    background-color: lightblue;
+  }
+  h3{
+    font-family: 'kanit', serif;
+    font-weight: 400;
+    font-size: 16px;
+    color:#FFFFFF;
+    line-height: 10px;
+  }
+
+`
+const SubTitle = styled.div`
+margin-left: 10%;
+width:100% ;
+height: 10px;
+margin-top: 40%;
+h4{
+    font-family: 'passione one', serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 10px;
+}
+
+
+  
+`
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,71 @@
+import styled from "styled-components";
+import logo from "../assets/imgs/logo.png";
+import FormsLoginSigin from "../components/FormsLoginSigin";
 
 export default function Login(){
     return(
-        <h1>oi</h1>
+        <>
+        <Conteiner>
+        <Left>
+            <h1>Welcome To Twillie</h1>
+            <p>the best place to organize your life</p>
+        </Left>
+        <Rigth>
+                    <img src={logo} alt="twillie logo" />
+                    <Inputs>
+
+                        <FormsLoginSigin
+                           type={"login"}
+                        />
+
+                    </Inputs>
+                </Rigth>
+        </Conteiner>
+        </>
     )
 }
+
+const Left = styled.div`
+display: flex;
+flex-direction: column;
+padding-top: 16%;
+padding-left: 5%;
+background-color: #5897D6;
+width: 60%;
+h1{
+    color: #f5fffb;
+    font-family: 'passione one', serif;
+    font-weight: 600;
+    font-size: 50px;
+    line-height: 58px;
+}
+p{
+    color: #f5fffb;
+    font-family: 'passione one', serif;
+    margin-left: 2%;
+    font-weight: 400;
+    font-size: 26px;
+    line-height: 10px;
+}
+
+`
+const Rigth = styled.div`
+background-color: #FFFFFF;
+width: 40%;
+display: flex;
+flex-direction: column;
+padding-top: 5%;
+align-items: center;
+img{
+    width: 90px;
+}
+`
+
+const Conteiner = styled.div`
+display: flex;
+width: 100%;
+height: 100vh;
+
+`
+const Inputs = styled.div`
+`
